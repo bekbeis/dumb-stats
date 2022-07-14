f9b5ff2dfb06de3669ac0177c493911498fe5c4c
bekbeis <bekzat.beis@gmail.com>
2022-07-14 18:03:27 +0600

    better ui

--- src/background/index.js
+++ src/background/index.js
@@ -1,9 +1,7 @@
-chrome.runtime.onInstalled.addListener(() => {
-  chrome.storage.local.set({totalLength: 0, clickCount: 0, keyPressCount: 0, pagesCount: 0});
-});
-
-// Executes ContentScript on all tabs (including already open ones)
 chrome.runtime.onInstalled.addListener(async () => {
+  chrome.storage.local.set({totalLength: 0, clickCount: 0, keyPressCount: 0, pagesCount: 0});
+
+  // Executes ContentScript on all tabs (including already open ones)
   for (const contentScript of chrome.runtime.getManifest().content_scripts) {
     for (const tab of await chrome.tabs.query({url: contentScript.matches})) {
       chrome.scripting.executeScript({
@@ -13,13 +11,5 @@ chrome.runtime.onInstalled.addListener(async () => {
     }
   }
 });
-
-// Executes ContentScript only on Updated/newly loaded tabs
-// chrome.tabs.onUpdated.addListener((tabId) => {
-//   chrome.scripting.executeScript({
-//     target: {tabId},
-//     files: ['content/index.js']
-//   });
-// });
 
 console.log('The dumb-stats project is successfully loaded!');
--- src/pages/popup/App.js
+++ src/pages/popup/App.js
@@ -1,12 +1,16 @@
 import React, { useState } from 'react';
+import Logo from './svgr/ds'
+import ClicksIcon from './svgr/click';
+import ScrollIcon from './svgr/scroll';
+import KeyPressIcon from './svgr/key-press';
+import PagesIcon from './svgr/page';
 
 const App = () => {
-
   const [scrollValue, setScrollValue] = useState(0);
   const [clicksValue, setClicksValue] = useState(0);
   const [keyPressValue, setKeyPressValue] = useState(0);
   const [pagesValue, setPagesValue] = useState(0);
-  
+
   chrome.storage.local.get(['totalLength', 'clickCount', 'keyPressCount', 'pagesCount'], (val) => {
     setScrollValue(val.totalLength);
     setClicksValue(val.clickCount);
@@ -14,28 +18,98 @@ const App = () => {
     setPagesValue(val.pagesCount);
   });
 
+  const resetStats = () => {
+    chrome.storage.local.set({totalLength: 0, clickCount: 0, keyPressCount: 0, pagesCount: 0});
+    chrome.storage.local.get(['totalLength', 'clickCount', 'keyPressCount', 'pagesCount'], (val) => {
+      setScrollValue(val.totalLength);
+      setClicksValue(val.clickCount);
+      setKeyPressValue(val.keyPressCount);
+      setPagesValue(val.pagesCount);
+    });
+  };
+
   const roundValue = (num) => {
     return Math.round((num + Number.EPSILON) * 100) / 100;
   };
 
+  const getKcal = (num) => {
+    return (num * 0.000000239).toExponential(3);
+  }
+  
   return (
-    <div>
-      <div className='heading'>
-        <h1>YOUR DUMB STATS!</h1>
-      </div>
-      <p>Total scroll distance: {roundValue(scrollValue)} m.</p>
-      <br />
-      <p>
-        Total clicks: {clicksValue}. You have burned {(clicksValue * 0.000000239).toExponential(3)} kcal by only clicking your mouse!
-      </p>
-      <br />
-      <p>
-        Total key presses: {keyPressValue}.
-      </p>
-      <br />
-      <p>
-        Total pages viewed: {pagesValue}.
-      </p>
+    <div className='main'>
+      <div className='header-container'>
+            <div id='logo'>
+                <Logo />
+            </div>
+            <div>
+                <button onClick={resetStats}>reset</button>
+            </div>
+        </div>
+        <div className='item-container'>
+            <div className='item'>
+                <div id="icon">
+                    <ClicksIcon />
+                </div>
+                <h1 className='name'>
+                    clicks
+                </h1>
+                <h1 className='val'>
+                    {clicksValue}
+                </h1>
+                <p className='description'>
+                    {`kcal burned by these clicks: ${getKcal(clicksValue)}`}
+                </p>
+            </div>
+        </div>
+        <div className='item-container'>
+            <div className='item'>
+                <div id="icon">
+                    <ScrollIcon />
+                </div>
+                <h1 className='name'>
+                    scroll distance
+                </h1>
+                <h1 className='val'>
+                    {roundValue(scrollValue)}
+                </h1>
+                <p className='description'>
+                    The value is in meters
+                </p>
+            </div>
+        </div>
+        <div className='item-container'>
+            <div className='item'>
+                <div id="icon">
+                    <KeyPressIcon />
+                </div>
+                <h1 className='name'>
+                    key presses
+                </h1>
+                <h1 className='val'>
+                    {keyPressValue}
+                </h1>
+                <p className='description'>
+                    Еще не придумал
+                </p>
+            </div>
+        </div>
+        <div className='item-container'>
+            <div className='item'>
+                <div id="icon">
+                    <PagesIcon />
+                </div>
+                <h1 className='name'>
+                    pages viewed
+                </h1>
+                <h1 className='val'>
+                    {pagesValue}
+                </h1>
+                <p className='description'>
+                    Еще не придумал x2
+                </p>
+            </div>
+        </div>
     </div>
   )
 };
