440256ef9782ab7c6331b7c309fa9a22b5bc8ece
bekbeis <bekzat.beis@gmail.com>
2022-07-14 18:04:25 +0600

    better design overall

--- dist/background/index.js
+++ dist/background/index.js
@@ -1,13 +1,11 @@
-chrome.runtime.onInstalled.addListener(() => {
+chrome.runtime.onInstalled.addListener(async () => {
   chrome.storage.local.set({
     totalLength: 0,
     clickCount: 0,
     keyPressCount: 0,
     pagesCount: 0
-  });
-}); // Executes ContentScript on all tabs (including already open ones)
+  }); // Executes ContentScript on all tabs (including already open ones)
 
-chrome.runtime.onInstalled.addListener(async () => {
   for (const contentScript of chrome.runtime.getManifest().content_scripts) {
     for (const tab of await chrome.tabs.query({
       url: contentScript.matches
@@ -20,12 +18,5 @@ chrome.runtime.onInstalled.addListener(async () => {
       });
     }
   }
-}); // Executes ContentScript only on Updated/newly loaded tabs
-// chrome.tabs.onUpdated.addListener((tabId) => {
-//   chrome.scripting.executeScript({
-//     target: {tabId},
-//     files: ['content/index.js']
-//   });
-// });
-
+});
 console.log('The dumb-stats project is successfully loaded!');
