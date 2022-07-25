1e6571681c66b49922eee04a9948e7dbdf7da6f9
bekbeis <bekzat.beis@gmail.com>
2022-07-25 10:02:40 +0600

    changed some functions

--- src/pages/popup/App.js
+++ src/pages/popup/App.js
@@ -28,19 +28,6 @@ const App = () => {
     : val.toExponential(3)
   );
 
-  const calculateTime = (seconds) => {
-    seconds = Number(seconds);
-    var d = Math.floor(seconds / (3600*24));
-    var h = Math.floor(seconds % (3600*24) / 3600);
-    var m = Math.floor(seconds % 3600 / 60);
-    var s = Math.floor(seconds % 60);
-    var dDisplay = d > 0 ? d + (d == 1 ? " day, " : " days, ") : "";
-    var hDisplay = h > 0 ? h + (h == 1 ? " hour, " : " hours, ") : "";
-    var mDisplay = m > 0 ? m + (m == 1 ? " minute, " : " minutes, ") : "";
-    var sDisplay = s > 0 ? s + (s == 1 ? " second" : " seconds") : "";
-    return dDisplay + hDisplay + mDisplay + sDisplay;
-  };
-
   const getData = () => {
     chrome.storage.local.get(['totalLength', 'clickCount', 'keyPressCount', 'pagesCount'], (val) => {
         setScrollValue(roundNum(val.totalLength));
@@ -57,31 +44,61 @@ const App = () => {
     getData();
   };
 
-  const getKcal = (val) => ( `you have burned nearly ${formatNum(val * 0.000000239)} calories in total by these clicks` );
-
-  const getDistance = (val) => (
-    (val < 384400000) ? `there are ${formatNum(384400000 - val)} more meters to reach the Moon`
-    : (val >= 384400000 && val < 384400010) ? `you are incredible! you have reached the Moon!`
-    : (val > 384400010 && val < 149597870700) ? `there are ${formatNum(149597870700 - val)} more meters to reach the Sun`
-    : (val >= 149597870700 && val < 149597870710) ? `you are even more incredible! you have reached the Sun!`
-    : (val > 149597870710 && val < 149597870720) ? `bro, you are going to infinity...`
-    : `you have reached the end`
+  const getKcal = (val) => (
+    val === 0 ? `Click to burn some calories`
+    : `You have burned nearly ${formatNum(val * 0.000000239)} calories in total by these clicks`
   );
+
+  const getDistance = (val) => ( // 
+    val === 0 ? `Scroll to start your road trip along the route Budapest-Lisbon`
+    : val < 242900 ? `There are ${formatNum(242900 - val)} more meters to get to Vienna from Budapest`
+    : val >= 242900 && val < 242930 ? `You are in Vienna`
+    : val >=242930 && val < 576430 ? `There are ${formatNum(576430 - val)} more meters to get to Prague from Vienna`
+    : val >= 576430 && val < 576460 ? `You are in Prague`
+    : val >= 576460 && val < 925860 ? `There are ${formatNum(925860 - val)} more meters to get to Berlin from Prague`
+    : val >= 925860 && val < 925890 ? `You are in Berlin`
+    : val >= 925890 && val < 1580631 ? `There are ${formatNum(1580631 - val)} more meters to get to Amsterdam from Berlin`
+    : val >= 1580631 && val < 1580661 ? `You are in Amsterdam`
+    : val >= 1580661 && val < 1790461 ? `There are ${formatNum(1790461 - val)} more meters to get to Brussels from Amsterdam`
+    : val >= 1790461 && val < 1790491 ? `You are in Brussels`
+    : val >= 1790491 && val < 2180091 ? `There are ${formatNum(2180091 - val)} more meters to get to Paris from Brussels`
+    : val >= 2180091 && val < 2180121 ? `You are in Paris`
+    : val >= 2180121 && val < 3455221 ? `There are ${formatNum(3455221 - val)} more meters to get to Madrid from Paris`
+    : val >= 3455221 && val < 3455251 ? `You are in Madrid`
+    : val >= 3455251 && val < 4079251 ? `There are ${formatNum(4079251 - val)} more meters to get to Lisbon from Madrid`
+    : val >= 4079251 && val < 4079301 ? `You've finished the Budapest-Lisbon road trip. The next stop is the Moon`
+    : val >= 4079301 && val < 388479301 ? `There are ${formatNum(388479301 - val)} more meters to reach the Moon`
+    : `You have reached the Moon. It is the END.`
+  );
+
+  const calculateTime = (seconds) => {
+    seconds = Number(seconds);
+    var d = Math.floor(seconds / (3600*24));
+    var h = Math.floor(seconds % (3600*24) / 3600);
+    var m = Math.floor(seconds % 3600 / 60);
+    var s = Math.floor(seconds % 60);
+    var dDisplay = d > 0 ? d + (d == 1 ? " day, " : " days, ") : "";
+    var hDisplay = h > 0 ? h + (h == 1 ? " hour, " : " hours, ") : "";
+    var mDisplay = m > 0 ? m + (m == 1 ? " minute, " : " minutes, ") : "";
+    var sDisplay = s > 0 ? s + (s == 1 ? " second" : " seconds") : "";
+    return dDisplay + hDisplay + mDisplay + sDisplay;
+  };
 
   const getCalculatedTime = (val) => {
     const time = calculateTime(54 * val);
-    return !time ? `nothing to display yet :(` 
-    : `you have spent approximately ` + time + ` on this`;
+    return !time ? `Visit any page to start collecting rough information about your time` 
+    : `You have spent approximately ` + time + ` on this`;
   };
 
-  const getNovelCount = (val) => (//727943
-    (val < 77710) ? `${77710 - val} more keystrokes and you could have finished typing The Little Prince`
-    : (val < 185650) ? `${185650 - val} more keystrokes and you could have finished typing Robinson Crusoe`
-    : (val < 632432) ? `${632432 - val} more keystrokes and you could have finished typing The Odyssey by Homer`
-    : (val < 994477) ? `${994477 - val} more keystrokes and you could have finished typing Crime and Punishment`
-    : (val < 1623333) ? `${1623333 - val} more keystrokes and you could have finished typing Don Quixote`
-    : (val < 2709358) ? `${2709358 - val} more keystrokes and you could have finished typing The Lord of the Rings`
-    : (val < 5095599) ? `${5095599 - val} more keystrokes and you could have finished typing the whole Harry Potter`
+  const getNovelCount = (val) => (
+    val === 0 ? `Press any key to know what novel you could have finished typing with your # of keystrokes`
+    : val < 77710 ? `${77710 - val} more keystrokes and you could have finished typing The Little Prince`
+    : val < 185650 ? `${185650 - val} more keystrokes and you could have finished typing Robinson Crusoe`
+    : val < 632432 ? `${632432 - val} more keystrokes and you could have finished typing The Odyssey by Homer`
+    : val < 994477 ? `${994477 - val} more keystrokes and you could have finished typing Crime and Punishment`
+    : val < 1623333 ? `${1623333 - val} more keystrokes and you could have finished typing Don Quixote`
+    : val < 2709358 ? `${2709358 - val} more keystrokes and you could have finished typing The Lord of the Rings trilogy`
+    : val < 5095599 ? `${5095599 - val} more keystrokes and you could have finished typing the whole Harry Potter`
     : `With this number of keystrokes, you could have finished typing ${Math.floor(val / 727943)} Harry Potter books`
   );
 
